e9f0fd13d86e7f2c615b9a27465a5e75ad3f0b8f
anandrajpandey <anandrajpandey75A@gmail.com>
2026-05-31 13:48:53 +0530

    region

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || "us-east-1"
+  region: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || "us-east-1"
 });
 
 export const db = DynamoDBDocumentClient.from(client);
